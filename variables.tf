4d92a4d8dcd336fb8921da367e64da1470dcfc90
vipin2060 <vipinvikraman86@gmail.com>
2024-09-18 09:01:19 +0800

    Uploaded variables.tf

---
+++ variables.tf
@@ -0,0 +1,18 @@
+#aws details 
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+
+variable  "profile_uuid" {}
+variable  "awslocation" {}
+variable "seller_region" {}
+variable "authentication_key" {}
+
+#ecx details 
+variable "ecx_access_key" {}
+variable "ecx_secret_key" {}
+variable "ecx_auth_token" {}
+variable "FCRtoAWSspeed" {}
+
+
+variable "email" {}
+variable "purchase_order_number" {}
