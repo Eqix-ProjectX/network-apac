99f8bf69f8ea033827de716b9979eb35376a7bbf
vipin2060 <vipinvikraman86@gmail.com>
2024-09-18 09:09:40 +0800

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,18 +1,9 @@
-#aws details 
-variable "aws_access_key" {}
-variable "aws_secret_key" {}
-
-variable  "profile_uuid" {}
-variable  "awslocation" {}
-variable "seller_region" {}
-variable "authentication_key" {}
-
-#ecx details 
-variable "ecx_access_key" {}
-variable "ecx_secret_key" {}
-variable "ecx_auth_token" {}
-variable "FCRtoAWSspeed" {}
-
-
-variable "email" {}
-variable "purchase_order_number" {}
+variable "FCRname" {}
+variable "FCRlocation" {}
+variable "FCRtype" {}
+variable "emails" {}
+variable "Equinixpurchaseorder" {}
+variable "FCRmetrocode" {}
+variable "FCRpackage" {}
+variable "Equinixprojectid" {}
+variable "account_number" {}
