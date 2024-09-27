8ef6710ab53ecbb58de0a73a52284a0b7b20b79a
vipin2060 <vipinvikraman86@gmail.com>
2024-09-27 17:46:53 +0800

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -9,10 +9,10 @@ variable "code" {}
 variable "project_id" {}
 variable "account_number" {}
 
-variable "emea_type" {}
-variable "emea_metro_code" {}
-variable "emea_emails" {}
-variable "emea_purchase_order_number" {}
-variable "emea_code" {}
-variable "emea_project_id" {}
-variable "emea_account_number" {}
+#variable "emea_type" {}
+#variable "emea_metro_code" {}
+#variable "emea_emails" {}
+#variable "emea_purchase_order_number" {}
+#variable "emea_code" {}
+#variable "emea_project_id" {}
+#variable "emea_account_number" {}
